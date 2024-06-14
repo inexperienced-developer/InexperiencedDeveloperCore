3ab01cbc46a4a2a07a556bac18f12fc4f83c4f48
inexperienced-developer <inexperienced.developer.games@gmail.com>
2024-06-14 09:53:45 -0400

    Added Util classes

---
+++ Runtime/Utils/Debug/StrippedDebug.cs
@@ -0,0 +1,35 @@
+using UnityEngine;  
+
+namespace InexperiencedDeveloper.Utils
+{
+    public static class StrippedDebug
+    {
+        public static void Log(string msg)
+        {
+#if UNITY_EDITOR
+            Debug.Log(msg);
+#endif
+        }
+
+        public static void LogWarning(string msg)
+        {
+#if UNITY_EDITOR
+            Debug.LogWarning(msg);
+#endif
+        }
+
+        public static void LogError(string msg)
+        {
+#if UNITY_EDITOR
+            Debug.LogError(msg);
+#endif
+        }
+
+        public static void Assert(Object obj)
+        {
+#if UNITY_EDITOR
+            Debug.Assert(obj != null);
+#endif
+        }
+    }
+}
